6502a76511c2b2414267d0adf1eb2f75a97df445
junindosite <juarezjunin2005@gmail.com>
2026-03-28 19:50:57 -0300

    add classe Usuario

---
+++ src/Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private Integer idade;
+    private String cidade;
+    private String telefone;
+}
