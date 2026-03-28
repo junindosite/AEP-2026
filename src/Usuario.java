d4308fccbe8632d58bc1ca0c01a2e76c5546cfd3
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-03-28 19:54:33 -0300

    Contrutor Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,4 +5,66 @@ public class Usuario {
     private Integer idade;
     private String cidade;
     private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Usuario(String nome, String cpf, String email, Integer idade, String cidade, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.idade = idade;
+        this.cidade = cidade;
+        this.telefone = telefone;
+
+
+
+    }
+
+
 }
