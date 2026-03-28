78844102e776476b4f5ef91cc8c3be17398e3eff
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-03-28 20:29:29 -0300

    Classe HistoricoStatus

---
+++ src/HistoricoStatus.java
@@ -0,0 +1,13 @@
+public class HistoricoStatus {
+    private String status;
+    private String comentario;
+    private String data;
+    private String responsavel;
+
+    public HistoricoStatus(String status, String comentario, String data, String responsavel) {
+        this.status = status;
+        this.comentario = comentario;
+        this.data = data;
+        this.responsavel = responsavel;
+    }
+}
