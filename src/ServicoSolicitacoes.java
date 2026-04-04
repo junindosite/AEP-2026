9515369e6fb4b04f4b387be6c0c7b2ef4a6c175c
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-04-04 18:52:51 -0300

    criando classe ServicoSollicitacoes

---
+++ src/ServicoSolicitacoes.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoSolicitacoes {
+
+    private List<Solicitacao> repositorio = new ArrayList<>();
+
+    public void novaSolicitacao(Solicitacao s) {
+        if (s != null) {
+            repositorio.add(s);
+            s.registrarMovimentacao(
+                    Status.ABERTO,
+                    "Solicitação criada pelo cidadão.",
+                    "Sistema"
+            );
+        }
+    }
+
+    public Solicitacao buscarPorProtocolo(int protocolo) {
+        return repositorio.stream()
+                .filter(s -> s.getProtocolo() == protocolo)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void listarSolicitacoes() {
+        for (Solicitacao s : repositorio) {
+            s.imprimirRelatorio();
+        }
+    }
+}
