62139de752bea07cd7e951c7cc84890f4a74aba5
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-04-04 19:02:11 -0300

    arrumando classe cidadao

--- src/Cidadao.java
+++ src/Cidadao.java
@@ -1,18 +1,18 @@
 public class Cidadao extends Usuario {
-
     private boolean anonimo;
 
-    public Cidadao(String nome, String cpf, String email, Integer idade, String cidade, String telefone, boolean anonimo) {
-        super(nome, cpf, email, idade, cidade, telefone);
+    public Cidadao(String nome, String cpf, String email, boolean anonimo) {
+        super(nome, cpf, email);
         this.anonimo = anonimo;
     }
 
     public boolean isAnonimo() {
         return anonimo;
     }
+
     public String getIdentidadeParaExibicao() {
         if (this.anonimo) {
-            return "USUÁRIO ANÔNIMO";
+            return "USUÁRIO ANÔNIMO)";
         }
         return getNome();
     }
